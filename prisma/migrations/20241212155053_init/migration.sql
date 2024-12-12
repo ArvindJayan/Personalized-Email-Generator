3190ee7d577972812d6335ae27155a9bb317a9f5
ArvindJayan <jayanarvind@gmail.com>
2024-12-12 21:42:20 +0530

    refactor: update dependencies

--- prisma/migrations/20241212155053_init/migration.sql
+++ prisma/migrations/20241212155053_init/migration.sql
@@ -1,9 +1,34 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "desc" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "expires_at" INTEGER NOT NULL,
+    "id_token" TEXT NOT NULL,
+    "scope" TEXT NOT NULL,
+    "session_state" TEXT NOT NULL,
+    "token_type" TEXT NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "token" TEXT NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier")
 );
 
 -- CreateTable
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -11,16 +10,6 @@ export default function Navigation() {
             <div className='flex text-gray-700'>
                 <Link className={path === '/' ? 'mx-2 space-x-4 font-medium bg-gray-300 rounded-lg shadow p-1 px-2' : 'mx-2 space-x-4 rounded-lg shadow p-1 px-2 hover:bg-gray-300 hover:shadow hover:text-gray-900'} href='/'>Home</Link>
                 <Link className={path === '/leads-and-products' ? 'mx-2 space-x-4 font-medium bg-gray-300 rounded-lg shadow p-1 px-2' : 'mx-2 space-x-4 rounded-lg shadow p-1 px-2 hover:bg-gray-300 hover:shadow hover:text-gray-900'} href='/leads-and-products'>Leads and Products</Link>
-
-                <SignedOut>
-                    <div className='mx-2 space-x-4 rounded-lg shadow p-1 px-2 hover:bg-gray-300 hover:shadow hover:text-gray-900'>
-                        <SignInButton mode='modal' />
-                    </div>
-                </SignedOut>
-
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
             </div>
         </nav >
     )
